4bee57d009c615e631734941f86c06d69e312303
Jakob Farian Krarup <kr@rup.dk>
2025-10-27 11:34:35 +0100

    Add project files.

---
+++ BlogSharp2025.DataAccessLibrary/IAuthorDao.cs
@@ -0,0 +1,16 @@
+﻿using BlogSharp2025.DataAccessLibrary.Model;
+
+namespace BlogSharp2025.DataAccessLibrary;
+
+public interface IAuthorDao
+{
+    Author GetOne(int id);
+    IEnumerable<Author> GetAll();
+    bool Delete(int id);
+    bool Update(Author author);
+    int Create(Author author);
+
+
+
+
+}
---
+++ BlogSharp2025.DataAccessLibrary/Model/Author.cs
@@ -0,0 +1,26 @@
+﻿namespace BlogSharp2025.DataAccessLibrary.Model;
+public class Author
+{
+    #region Properties
+    public int Id { get; internal set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string BlogTitle { get; set; }
+    #endregion
+
+    #region Constructors
+    public Author(int id, string email, string passwordHash, string blogTitle)
+    {
+        Id = id;
+        Email = email;
+        PasswordHash = passwordHash;
+        BlogTitle = blogTitle;
+    }
+
+    public Author(string email, string blogTitle)
+    {
+        Email = email;
+        BlogTitle = blogTitle;
+    } 
+    #endregion
+}
